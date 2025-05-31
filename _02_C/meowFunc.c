ba1f921f1d21871165f66d33b2e484eb2779ba51
anurag <016.krsna@gmail.com>
2025-05-31 17:13:21 +0530

    Updated C

--- _02_C/meowFunc.c
+++ _02_C/meowFunc.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
+
+void meow(int howManyTimes);
+
+int main(void) {
+    meow(10);
+}
+
 void meow(int howManyTimes) {
     for (int i = 0; i < howManyTimes; ++i) {
         printf("Meow!");
     }
     printf("\n");
-}
-
-int main(void) {
-    meow(10);
 }
