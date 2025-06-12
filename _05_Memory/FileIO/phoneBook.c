ef30c46ef5b5a1b484307e288d450dcc8f4c8268
anurag <016.krsna@gmail.com>
2025-06-12 07:53:55 +0530

    Updated C

---
+++ _05_Memory/FileIO/phoneBook.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void) {
+    FILE *file = fopen("phonebook.csv", "a");
+
+    if (file == NULL) {
+        return 1;
+    }
+
+    char *name = get_string("Name: ");
+    char* num = get_string("Number: ");
+    fprintf(file, "%s, %s\n", name, num);
+    fclose(file);
+}
